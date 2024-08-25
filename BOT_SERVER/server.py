e5307b9fd1a20f4fc6713a62534fe62a8111b1ff
huanpham123 <158818634+huanpham123@users.noreply.github.com>
2024-08-25 13:29:16 +0700

    Add files via upload

---
+++ BOT_SERVER/server.py
@@ -0,0 +1,65 @@
+# from flask import Flask, request, jsonify, send_from_directory
+# from flask_cors import CORS
+# from g4f.client import Client
+# import os
+
+# app = Flask(__name__)
+# CORS(app)
+# client = Client()
+
+# @app.route('/')
+# def index():
+#     return send_from_directory('.', 'index.html')
+
+# @app.route('/chat', methods=['POST'])
+# def chat():
+#     data = request.json
+#     user_input = data.get('message')
+#     response = client.chat.completions.create(
+#         model="gpt-4",
+#         messages=[{"role": "user", "content": user_input}],
+#     )
+#     reply = response.choices[0].message.content
+#     return jsonify({'reply': reply})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
+from g4f.client import Client
+import os
+
+app = Flask(__name__)
+CORS(app)
+client = Client()
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.json
+    user_input = data.get('message')
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=[{"role": "user", "content": user_input}],
+    )
+    reply = response.choices[0].message.content
+    return jsonify({'reply': reply})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
