400ce7c2a9fa79c8fe9681a7a8de0d6317ac4650
huanpham123 <158818634+huanpham123@users.noreply.github.com>
2024-08-25 13:27:35 +0700

    Update server.py

--- server.py
+++ server.py
@@ -23,4 +23,4 @@ def chat():
     return jsonify({'reply': reply})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
